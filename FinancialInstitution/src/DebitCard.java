0bb9e43fa7dc6bc6c8f35fc6b2cba74eb562f5b3
DenysVodzyk <vodzyk@gmail.com>
2020-12-29 22:57:40 -0500

    Class for Debit Card

---
+++ FinancialInstitution/src/DebitCard.java
@@ -0,0 +1,15 @@
+public class DebitCard {
+    private int balanceOnDebitCard;
+
+    public DebitCard(int balanceOnDebitCard){
+        this.balanceOnDebitCard = balanceOnDebitCard;
+    }
+
+    public int getBalanceOnDebitCard() {
+        return balanceOnDebitCard;
+    }
+
+    public void setBalanceOnDebitCard(int balanceOnDebitCard) {
+        this.balanceOnDebitCard = balanceOnDebitCard;
+    }
+}
