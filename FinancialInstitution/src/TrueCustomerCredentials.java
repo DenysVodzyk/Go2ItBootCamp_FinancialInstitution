72ae6d450ca6d8b68b378f73157e5757ac743e29
DenysVodzyk <vodzyk@gmail.com>
2020-12-29 22:58:55 -0500

    Class for customer verification

---
+++ FinancialInstitution/src/TrueCustomerCredentials.java
@@ -0,0 +1,25 @@
+public class TrueCustomerCredentials {
+    private String authenticCustomerName;
+    private String authenticCustomerPassword;
+
+    public TrueCustomerCredentials(String name, String password) {
+        this.authenticCustomerName = name;
+        this.authenticCustomerPassword = password;
+    }
+
+    public String getAuthenticCustomerName() {
+        return authenticCustomerName;
+    }
+
+    public void setAuthenticCustomerName(String authenticCustomerName) {
+        this.authenticCustomerName = authenticCustomerName;
+    }
+
+    public String getAuthenticCustomerPassword() {
+        return authenticCustomerPassword;
+    }
+
+    public void setAuthenticCustomerPassword(String authenticCustomerPassword) {
+        this.authenticCustomerPassword = authenticCustomerPassword;
+    }
+}
