53877f775c594784da382e706521647d7b152a97
DenysVodzyk <vodzyk@gmail.com>
2020-12-29 22:55:55 -0500

    Class for Credit Card

---
+++ FinancialInstitution/src/CreditCard.java
@@ -0,0 +1,15 @@
+public class CreditCard {
+    private int creditCardLimit;
+
+    public CreditCard(int creditCardLimit){
+        this.creditCardLimit = creditCardLimit;
+    }
+
+    public int getCreditCardLimit() {
+        return creditCardLimit;
+    }
+
+    public void setCreditCardLimit(int creditCardLimit) {
+        this.creditCardLimit = creditCardLimit;
+    }
+}
