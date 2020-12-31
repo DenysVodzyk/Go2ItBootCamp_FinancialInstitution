eaa87f5951989d4fe366aa8ce1d6f5c78e7c8b2c
DenysVodzyk <vodzyk@gmail.com>
2020-12-30 20:16:37 -0500

    Improved functionality of the class

--- FinancialInstitution/src/CreditCard.java
+++ FinancialInstitution/src/CreditCard.java
@@ -1,15 +1,16 @@
 public class CreditCard {
-    private int creditCardLimit;
+    private int creditCardBalance;
 
     public CreditCard(int creditCardLimit){
-        this.creditCardLimit = creditCardLimit;
+        this.creditCardBalance = creditCardLimit;
     }
 
-    public int getCreditCardLimit() {
-        return creditCardLimit;
+    public int getCreditCardBalance() {
+        return creditCardBalance;
     }
 
-    public void setCreditCardLimit(int creditCardLimit) {
-        this.creditCardLimit = creditCardLimit;
+    public void setCreditCardBalance(int creditCardBalance) {
+        this.creditCardBalance = creditCardBalance;
     }
+
 }
