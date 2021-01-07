9727554e2cc222ba305e9fbf8cf201bc719f976b
DenysVodzyk <vodzyk@gmail.com>
2021-01-06 22:39:41 -0500

    Modified method

--- FinancialInstitution/src/Entity/FamilyMember.java
+++ FinancialInstitution/src/Entity/FamilyMember.java
@@ -1,38 +1,19 @@
-import java.time.LocalDate;
-import java.util.Objects;
+package Entity;
 
-public class FamilyMember {
-    private String name;
-    private LocalDate dOb;
+import java.time.LocalDate;
+
+public class FamilyMember extends Person {
     private String relationship;
 
     public FamilyMember(String name, LocalDate dOb, String relationship) {
-        this.name = name;
-        this.dOb = dOb;
+        super(name, dOb);
         this.relationship = relationship;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FamilyMember that = (FamilyMember) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(dOb, that.dOb) &&
-                Objects.equals(relationship, that.relationship);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, dOb, relationship);
-    }
-
-    @Override
     public String toString() {
-        return "FamilyMember{" +
-                "name='" + name + '\'' +
-                ", dOb=" + dOb +
-                ", relationship='" + relationship + '\'' +
+        return "FamilyMember{" + super.toString() +
+                " relationship='" + relationship + '\'' +
                 '}';
     }
 }
