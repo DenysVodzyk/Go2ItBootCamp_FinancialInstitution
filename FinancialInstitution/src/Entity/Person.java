e2e7a7a796426e14bf72bcaa810e4a7fc9f8abb0
DenysVodzyk <vodzyk@gmail.com>
2021-01-06 22:37:06 -0500

    Modified method

---
+++ FinancialInstitution/src/Entity/Person.java
@@ -0,0 +1,37 @@
+package Entity;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private LocalDate dOb;
+
+    public Person(String name, LocalDate dOb) {
+        this.name = name;
+        this.dOb = dOb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getdOb() {
+        return dOb;
+    }
+
+    public void setdOb(LocalDate dOb) {
+        this.dOb = dOb;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", dOb=" + dOb +
+                '}';
+    }
+}
