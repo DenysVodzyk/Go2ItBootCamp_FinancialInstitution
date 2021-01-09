4daaa2f3250db656af99836e26ed9e161c42dc48
DenysVodzyk <vodzyk@gmail.com>
2021-01-08 21:38:39 -0500

    Minor changes

--- FinancialInstitution/src/Service/CreditLineService.java
+++ FinancialInstitution/src/Service/CreditLineService.java
@@ -1,14 +1,56 @@
-public class CreditLineService {
+package Service;
 
-    public CreditLineService() {
+import Exception.*;
+import Entity.*;
+
+public class CreditLineService extends FinancialInstitutionProductService {
+    private CreditLine creditLine;
+    private double rate;
+    private static final double CANADIAN_RATE = 20;
+    private static final double NON_CANADIAN_RATE = 22;
+
+    public CreditLineService(Customer customer, CreditLine creditLine) {
+        super(customer);
+        this.creditLine = creditLine;
     }
 
-    public boolean isCustomerEligibleForCreditLine(Customer customer) throws AccessDeniedException {
-        if (customer.getCreditScore() < 650) {
-            throw new AccessDeniedException("Customer is not eligible for credit line. His/her credit score is less than 650.");
+    public double calculateRate() throws AccessDeniedException {
+        if (!creditLine.isCustomerEligibleForCreditLine()) {
+            throw new AccessDeniedException("Access denied.");
         }
-        System.out.println("Customer is eligible for credit line!");
-        return true;
+        if (getCustomer().isCanadian()) {
+            setRate(CANADIAN_RATE);
+            System.out.println("Your line of credit interest will be " + rate + "%");
+            return rate;
+        } else {
+            setRate(NON_CANADIAN_RATE);
+            System.out.println("Your line of credit interest will be " + rate + "%");
+            return rate;
+        }
     }
 
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public boolean isEligibleForPromotion() {
+        if (getCustomer().getAmountSpentLastMonth() >= 600000) {
+            System.out.println("Customer is eligible to receive 1% reduction to his line of credit rate.");
+            return true;
+        }
+        System.out.println("Customer is not eligible for promotion.");
+        return false;
+    }
+
+    @Override
+    public void applyPromotion() {
+        if (isEligibleForPromotion()) {
+            setRate(rate - 1);
+        }
+    }
 }
