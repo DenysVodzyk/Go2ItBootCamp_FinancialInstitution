81f7e817cf0c2c69316762ad81de166f10e6a941
DenysVodzyk <vodzyk@gmail.com>
2021-01-09 21:16:39 -0500

    Methods modifications

--- FinancialInstitution/src/Service/CustomerService.java
+++ FinancialInstitution/src/Service/CustomerService.java
@@ -1,18 +1,15 @@
+package Service;
+
+import Exception.*;
+import Entity.*;
+
 public class CustomerService {
 
-
-    public void addFamilyMember(Customer customer, FamilyMember familyMember) throws AccessDeniedException {
-        if (!customer.isConsentToProvideFamilyMemberDetails()) {
-            throw new AccessDeniedException("You don't have customer permission to add family members.");
-        }
-        customer.getFamilyMembers().add(familyMember);
-    }
-
-    public boolean isCustomerCredentialValid(Customer customer) throws AccessDeniedException {
+    public static boolean isCustomerCredentialValid(Customer customer) throws AccessDeniedException {
         String customerGivenName = customer.getName();
-        String customerAuthenticName = customer.getTrueCustomerCredentials().getAuthenticCustomerName();
+        String customerAuthenticName = customer.getCustomerCredentials().getAuthenticCustomerName();
         String customerGivenPassword = customer.getPassword();
-        String customerAuthenticPassword = customer.getTrueCustomerCredentials().getAuthenticCustomerPassword();
+        String customerAuthenticPassword = customer.getCustomerCredentials().getAuthenticCustomerPassword();
 
         if (!customerGivenName.equals(customerAuthenticName) || !customerGivenPassword.equals(customerAuthenticPassword)) {
             throw new AccessDeniedException("Access denied. Invalid Username or Password");
@@ -21,12 +18,17 @@ public class CustomerService {
         return true;
     }
 
-    public boolean isCustomerEligibleForPromotion(Customer customer) throws AccessDeniedException {
-        if (customer.getAmountSpentLastMonth() < 500000) {
-            throw new AccessDeniedException("Customer is not eligible for promotion. He/she hasn't spent $5000 last month.");
-        }
-        System.out.println("Customer is eligible for promotion!");
-        return true;
+    public static void addFinancialProduct(Customer customer, FinancialInstitutionProduct financialInstitutionProduct){
+        customer.getFinancialInstitutionProducts().add(financialInstitutionProduct);
     }
+
+    public static void addFamilyMember(Customer customer, FamilyMember familyMember) throws AccessDeniedException {
+        if (!customer.isAgreeToShareFamilyInfo()) {
+            throw new AccessDeniedException("You don't have customer permission to add family members.");
+        }
+        customer.getFamilyMembers().add(familyMember);
+    }
+
+
 
 }
