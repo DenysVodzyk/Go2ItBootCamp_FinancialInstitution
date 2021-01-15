e6cbd6d1314f55c629404ce02a946b42ca838815
DenysVodzyk <vodzyk@gmail.com>
2021-01-14 19:39:28 -0500

    Test methods refactor

--- FinancialInstitution/test/Service/DebitCardServiceTest.java
+++ FinancialInstitution/test/Service/DebitCardServiceTest.java
@@ -24,14 +24,14 @@ class DebitCardServiceTest {
 
         debitCard1 = new DebitCard(customer1, 5000);
         debitCard2 = new DebitCard(customer2, 0);
-        debitCardService = new DebitCardService(customer1, debitCard1);
+        debitCardService = new DebitCardService();
     }
 
 
     @Test
     public void withdrawMoneyFromDebitCardTrueTest() {
         try {
-            debitCardService.withdrawMoneyFromDebitCard(100);
+            debitCardService.withdrawMoneyFromDebitCard(debitCard1,100);
             assertEquals(4900, debitCard1.getBalance());
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,7 +41,7 @@ class DebitCardServiceTest {
     @Test
     public void withdrawMoneyFromDebitCardFalseTest() {
         try {
-            debitCardService.withdrawMoneyFromDebitCard(5100);
+            debitCardService.withdrawMoneyFromDebitCard(debitCard1,5100);
         } catch (Exception e) {
             e.printStackTrace();
             assertEquals(e.getClass(), LimitExceededException.class);
@@ -50,14 +50,14 @@ class DebitCardServiceTest {
 
     @Test
     public void depositMoneyToDebitCardTrueTest() {
-        debitCardService.depositMoneyToDebitCard(1000);
+        debitCardService.depositMoneyToDebitCard(debitCard1,1000);
         assertEquals(6000, debitCard1.getBalance());
     }
 
     @Test
     public void depositMoneyToDebitCardFalseTest() {
         try {
-            debitCardService.depositMoneyToDebitCard(-1);
+            debitCardService.depositMoneyToDebitCard(debitCard1,-1);
         } catch (Exception e) {
             e.getMessage();
             assertEquals(e.getClass(), IllegalArgumentException.class);
@@ -67,7 +67,7 @@ class DebitCardServiceTest {
     @Test
     public void transferToDifferentDebitCardTrueTest() {
         try {
-            debitCardService.transferToDifferentDebitCard(1000, debitCard2);
+            debitCardService.transferToDifferentDebitCard(debitCard1, debitCard2, 1000);
             System.out.println("Debit Card #1 balance after transfer: " + debitCard1.getBalance());
             assertEquals(1000, debitCard2.getBalance());
         } catch (LimitExceededException e) {
@@ -78,7 +78,7 @@ class DebitCardServiceTest {
     @Test
     public void transferToDifferentDebitCardFalseTest() {
         try {
-            debitCardService.transferToDifferentDebitCard(6000, debitCard2);
+            debitCardService.transferToDifferentDebitCard(debitCard1, debitCard2, 6000);
         } catch (Exception e) {
             e.printStackTrace();
             assertEquals(e.getClass(), LimitExceededException.class);
