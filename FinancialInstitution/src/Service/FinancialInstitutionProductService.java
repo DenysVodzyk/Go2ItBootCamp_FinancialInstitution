b7bc0636bd061066b6436e906ff64e34b6abf6cf
DenysVodzyk <vodzyk@gmail.com>
2021-01-14 19:35:53 -0500

    Minor changes

--- FinancialInstitution/src/Service/FinancialInstitutionProductService.java
+++ FinancialInstitution/src/Service/FinancialInstitutionProductService.java
@@ -1,20 +1,5 @@
 package Service;
 
-import Entity.Customer;
-
 public abstract class FinancialInstitutionProductService {
-    private Customer customer;
-
-    public FinancialInstitutionProductService(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
 
 }
